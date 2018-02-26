65b88bddd4ad7e8426bcf767299ce57782869f71
laila <laila@fake.com>
2018-02-27 00:45:59 +0200

    added more elements to xml file

--- src/CreateXML.java
+++ src/CreateXML.java
@@ -1,3 +1,4 @@
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -11,6 +12,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+
 public class CreateXML {
 
     public static void main(String[] args){
@@ -23,7 +25,43 @@ public class CreateXML {
             // creating root element
             Element rootElement = doc.createElement("CATALOG");
             doc.appendChild(rootElement);
+            //creating CD element
+            Element cd = doc.createElement("CD");
+            rootElement.appendChild(cd);
+            //creating attribute for CD element
+            Attr attr_cd = doc.createAttribute("id");
+            attr_cd.setValue("88");
+            cd.setAttributeNode(attr_cd);
 
+            //creating Title element
+            Element title = doc.createElement("TITLE");
+            title.appendChild(doc.createTextNode("Nevermind"));
+            cd.appendChild(title);
+            //creating artist element
+            Element artist = doc.createElement("ARTIST");
+            artist.appendChild(doc.createTextNode("Nirvana"));
+            cd.appendChild(artist);
+            //creating Country element
+            Element country = doc.createElement("COUNTRY");
+            country.appendChild(doc.createTextNode("USA"));
+            cd.appendChild(country);
+
+            //creating Company element
+            Element company = doc.createElement("COMPANY");
+            company.appendChild(doc.createTextNode("Butch Vig"));
+            cd.appendChild(company);
+
+            //creating Price element
+            Element price = doc.createElement("PRICE");
+            price.appendChild(doc.createTextNode("10.90"));
+            cd.appendChild(price);
+            //creating Year element
+            Element year = doc.createElement("YEAR");
+            year.appendChild(doc.createTextNode("1991"));
+            cd.appendChild(year);
+
+            //cretaing the 2nd CD element
+            rootElement.appendChild(cd);
             //writing into file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
