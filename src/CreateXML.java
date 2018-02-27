75d9cf2ea37e28ef25f279baeb052ceb3278012c
laila <laila@fake.com>
2018-02-27 20:59:56 +0200

    added a function which create the cd element

--- src/CreateXML.java
+++ src/CreateXML.java
@@ -14,54 +14,51 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.File;
 
 public class CreateXML {
-
+    static Element generateCdEntry(Document doc,String Title, String Artist,String Year,String Country,String Price,String id,String Company){
+        Element cd = doc.createElement("CD");
+        //creating attribute for CD element
+        Attr attr_cd = doc.createAttribute("id");
+        attr_cd.setValue(id);
+        cd.setAttributeNode(attr_cd);
+        //creating Title element
+        Element title = doc.createElement("TITLE");
+        title.appendChild(doc.createTextNode(Title));
+        cd.appendChild(title);
+        //creating artist element
+        Element artist = doc.createElement("ARTIST");
+        artist.appendChild(doc.createTextNode(Artist));
+        cd.appendChild(artist);
+        //creating Country element
+        Element country = doc.createElement("COUNTRY");
+        country.appendChild(doc.createTextNode(Country));
+        cd.appendChild(country);
+        //creating Company element
+        Element company = doc.createElement("COMPANY");
+        company.appendChild(doc.createTextNode(Company));
+        cd.appendChild(company);
+        //creating Price element
+        Element price = doc.createElement("PRICE");
+        price.appendChild(doc.createTextNode(Price));
+        cd.appendChild(price);
+        //creating Year element
+        Element year = doc.createElement("YEAR");
+        year.appendChild(doc.createTextNode(Year));
+        cd.appendChild(year);
+        return cd;
+    }
     public static void main(String[] args){
 
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.newDocument();
-
             // creating root element
             Element rootElement = doc.createElement("CATALOG");
             doc.appendChild(rootElement);
-            //creating CD element
-            Element cd = doc.createElement("CD");
-            rootElement.appendChild(cd);
-            //creating attribute for CD element
-            Attr attr_cd = doc.createAttribute("id");
-            attr_cd.setValue("88");
-            cd.setAttributeNode(attr_cd);
 
-            //creating Title element
-            Element title = doc.createElement("TITLE");
-            title.appendChild(doc.createTextNode("Nevermind"));
-            cd.appendChild(title);
-            //creating artist element
-            Element artist = doc.createElement("ARTIST");
-            artist.appendChild(doc.createTextNode("Nirvana"));
-            cd.appendChild(artist);
-            //creating Country element
-            Element country = doc.createElement("COUNTRY");
-            country.appendChild(doc.createTextNode("USA"));
-            cd.appendChild(country);
+            rootElement.appendChild(generateCdEntry(doc,"Nevermind","Nirvana","1991","USA","10.90","88","Butch Vig"));
+            rootElement.appendChild(generateCdEntry(doc,"No Need to Argue","The Cranberries","1994","UK","9.99","77","CBS Records"));
 
-            //creating Company element
-            Element company = doc.createElement("COMPANY");
-            company.appendChild(doc.createTextNode("Butch Vig"));
-            cd.appendChild(company);
-
-            //creating Price element
-            Element price = doc.createElement("PRICE");
-            price.appendChild(doc.createTextNode("10.90"));
-            cd.appendChild(price);
-            //creating Year element
-            Element year = doc.createElement("YEAR");
-            year.appendChild(doc.createTextNode("1991"));
-            cd.appendChild(year);
-
-            //cretaing the 2nd CD element
-            rootElement.appendChild(cd);
             //writing into file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
