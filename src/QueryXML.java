324ac6b14dd057dc60f5aa6ed44cf8b40f815406
laila <laila@fake.com>
2018-02-27 23:20:12 +0200

    quarying artists name

--- src/QueryXML.java
+++ src/QueryXML.java
@@ -1,4 +1,6 @@
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -20,10 +22,29 @@ public class QueryXML {
             Document doc = docBuilder.parse(my_xmlFile);
             doc.getDocumentElement().normalize();
 
-            System.out.println(doc.getDocumentElement().getNodeName());
+            //System.out.println(doc.getDocumentElement().getNodeName());
             NodeList nList = doc.getElementsByTagName("CD");
 
+            for(int i=0;i<nList.getLength();i++){
+                Node node = nList.item(i);
+              //  System.out.println(node.getNodeName());
 
+                if(node.getNodeType()==Node.ELEMENT_NODE){
+                    Element nNode = (Element) node;
+                   // System.out.println(nNode.getAttribute("id"));
+                    NodeList artists = nNode.getElementsByTagName("ARTIST");
+
+                    for(int j=0;j<artists.getLength();j++){
+                        Node n = artists.item(j);
+
+                        if(n.getNodeType()==n.ELEMENT_NODE){
+                            Element artist = (Element) n;
+                            System.out.println("artist name: "+artist.getTextContent());
+                        }
+                    }
+
+                }
+            }
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (SAXException e) {
